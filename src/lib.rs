bf895ddb4e25a044211c75ddb9e9d0225e2e18e0
Vasco Costa <vascomacosta@gmail.com>
2023-09-11 10:08:17 +0100

    Add Config struct

--- src/lib.rs
+++ src/lib.rs
@@ -3,34 +3,40 @@ use serde::{Deserialize, Serialize};
 use std::{error::Error, path::Path, sync::Arc};
 use tokio::{task, task::JoinError};
 
-pub struct Database<PA: AsRef<Path> + Send + Sync + Clone> {
+pub struct Config<PA> {
     path: PA,
     extension: String,
 }
+pub struct Database<PA> {
+    config: Arc<Config<PA>>,
+}
 
-impl<PA: AsRef<Path> + Send + Sync + Clone + 'static> Database<PA> {
+impl<PA> Database<PA>
+where
+    PA: AsRef<Path> + Send + Sync + Clone + 'static,
+{
     pub fn new(path: PA, extension: Option<&str>) -> Self {
         Self {
-            path,
-            extension: String::from(extension.unwrap_or("csv")),
+            config: Arc::new(Config {
+                path,
+                extension: String::from(extension.unwrap_or("csv")),
+            }),
         }
     }
 
-    pub async fn find<T, P>(
-        self: Arc<Self>,
-        collection: &str,
-        predicate: P,
-    ) -> Result<Vec<T>, Box<dyn Error>>
+    pub async fn find<T, P>(&self, collection: &str, predicate: P) -> Result<Vec<T>, Box<dyn Error>>
     where
         T: Serialize + for<'de> Deserialize<'de> + Send + 'static,
         P: FnMut(&T) -> bool,
     {
         let collection = collection.to_string();
+        let config = self.config.clone();
         let results: Result<Result<Vec<T>, _>, _> = task::spawn_blocking(move || {
             let mut rdr = Reader::from_path(
-                self.path
+                config
+                    .path
                     .as_ref()
-                    .join(format!("{}.{}", collection, self.extension)),
+                    .join(format!("{}.{}", collection, config.extension)),
             )?;
 
             rdr.deserialize().collect()
@@ -40,21 +46,19 @@ impl<PA: AsRef<Path> + Send + Sync + Clone + 'static> Database<PA> {
         Ok(results??.into_iter().filter(predicate).collect())
     }
 
-    pub async fn insert<T>(
-        self: Arc<Self>,
-        collection: &str,
-        document: T,
-    ) -> Result<(), Box<dyn Error>>
+    pub async fn insert<T>(&self, collection: &str, document: T) -> Result<(), Box<dyn Error>>
     where
         T: Serialize + for<'de> Deserialize<'de> + Send + 'static,
     {
         let documents: Vec<T> = self.clone().find(collection, |_| true).await?;
         let collection = collection.to_string();
+        let config = self.config.clone();
         let result: Result<Result<(), csv::Error>, JoinError> = task::spawn_blocking(move || {
             let mut wrt = Writer::from_path(
-                self.path
+                config
+                    .path
                     .as_ref()
-                    .join(format!("{}.{}", collection, self.extension)),
+                    .join(format!("{}.{}", collection, config.extension)),
             )?;
 
             for document in documents {
@@ -71,7 +75,7 @@ impl<PA: AsRef<Path> + Send + Sync + Clone + 'static> Database<PA> {
     }
 
     pub async fn delete<T, P>(
-        self: Arc<Self>,
+        &self,
         collection: &str,
         mut predicate: P,
     ) -> Result<(), Box<dyn Error>>
@@ -81,14 +85,16 @@ impl<PA: AsRef<Path> + Send + Sync + Clone + 'static> Database<PA> {
     {
         let mut documents: Vec<T> = self.clone().find(collection, |_| true).await?;
         let collection = collection.to_string();
+        let config = self.config.clone();
 
         documents.retain(|d| !predicate(&d));
 
         let result: Result<Result<(), csv::Error>, JoinError> = task::spawn_blocking(move || {
             let mut wrt = Writer::from_path(
-                self.path
+                config
+                    .path
                     .as_ref()
-                    .join(format!("{}.{}", collection, self.extension)),
+                    .join(format!("{}.{}", collection, config.extension)),
             )?;
 
             for document in &documents {
@@ -103,7 +109,7 @@ impl<PA: AsRef<Path> + Send + Sync + Clone + 'static> Database<PA> {
     }
 
     pub async fn update<T, P>(
-        self: Arc<Self>,
+        &self,
         collection: &str,
         document: T,
         mut predicate: P,
@@ -114,14 +120,16 @@ impl<PA: AsRef<Path> + Send + Sync + Clone + 'static> Database<PA> {
     {
         let mut documents: Vec<T> = self.clone().find(collection, |_| true).await?;
         let collection = collection.to_string();
+        let config = self.config.clone();
 
         documents.retain(|d| !predicate(&d));
 
         let result: Result<Result<(), csv::Error>, JoinError> = task::spawn_blocking(move || {
             let mut wrt = Writer::from_path(
-                self.path
+                config
+                    .path
                     .as_ref()
-                    .join(format!("{}.{}", collection, self.extension)),
+                    .join(format!("{}.{}", collection, config.extension)),
             )?;
 
             for document in documents {
